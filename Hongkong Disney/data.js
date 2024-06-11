51f2a0b4fa8cc33fb341fca4696e03ffc6885b23
sarapeegaiying <160296150+sarapeegaiying@users.noreply.github.com>
2024-06-11 16:07:31 +0800

    3

--- Hongkong Disney/data.js
+++ Hongkong Disney/data.js
@@ -352,7 +352,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
